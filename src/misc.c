789875e78d815e34bf5b6d1d00c53b35cdc7cb98
Jujumba <far77578@gmail.com>
2023-04-25 14:03:46 +0200

    Delete misc.h & misc.c

--- src/misc.c
+++
@@ -1,9 +0,0 @@
-#include "misc.h"
-
-void panic(char *msg, ...) { // Definitely not stolen from the Rust™
-    va_list args;
-    va_start(args, msg);
-    fprintf(stderr, msg, args);
-    va_end(args);
-    exit(EXIT_FAILURE);
-}
--- src/misc.h
+++
@@ -1,6 +0,0 @@
-#ifndef PANIC_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h> 
-void panic(char*,...);
-#endif // PANIC_H
