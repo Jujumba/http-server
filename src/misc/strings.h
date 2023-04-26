06416e2d5e0b260eef3629e621c03998d6935796
Jujumba <far77578@gmail.com>
2023-04-26 14:09:24 +0200

    strings: name parameters

--- src/misc/strings.h
+++ src/misc/strings.h
@@ -4,6 +4,6 @@
 #include <stddef.h>
 #include "panic.h"
 
-void concat_strings(char *restrict, char *restrict, size_t *restrict, size_t *restrict);
+void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len, size_t *restrict src_len);
 
 #endif //HTTP_SERVER_STRINGS_H
