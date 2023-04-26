04eb614e655cb646965da09635b184a98e89fd54
Jujumba <far77578@gmail.com>
2023-04-26 14:11:13 +0200

    strings: inline `src_len` parameter

--- src/misc/strings.c
+++ src/misc/strings.c
@@ -5,7 +5,8 @@ static void resize(size_t *restrict a, const size_t *restrict b) {
         (*a) *= 2;
     }
 }
-void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len, size_t *restrict src_len) {
+void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len) {
+    size_t src_len = strlen(src);
     resize(dst_len, src_len);
     dst = realloc(dst, *dst_len);
     if (!dst) {
--- src/misc/strings.h
+++ src/misc/strings.h
@@ -2,8 +2,9 @@
 #define HTTP_SERVER_STRINGS_H
 
 #include <stddef.h>
+#include <string.h>
 #include "panic.h"
 
-void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len, size_t *restrict src_len);
+void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len);
 
 #endif //HTTP_SERVER_STRINGS_H
