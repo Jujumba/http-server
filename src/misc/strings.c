819e5ec1b1c61f0795abb48ac8721d98713ad1d9
Jujumba <far77578@gmail.com>
2023-04-26 13:00:18 +0200

    Move string concatenation to strings.(c && h)

---
+++ src/misc/strings.c
@@ -0,0 +1,15 @@
+#include "strings.h"
+
+static void resize(size_t *restrict a, const size_t *restrict b) {
+    while (*a <= *b) {
+        (*a) *= 2;
+    }
+}
+void concat_strings(char *restrict dst, char *restrict src, size_t *restrict dst_len, size_t *restrict src_len) {
+    resize(dst_len, src_len);
+    dst = realloc(dst, *dst_len);
+    if (!dst) {
+        PANIC("Malloc error\n");
+    }
+    snprintf(src, *src_len, "%s%s", src, dst);
+}
---
+++ src/misc/strings.h
@@ -0,0 +1,9 @@
+#ifndef HTTP_SERVER_STRINGS_H
+#define HTTP_SERVER_STRINGS_H
+
+#include <stddef.h>
+#include "panic.h"
+
+void concat_strings(char *restrict, char *restrict, size_t *restrict, size_t *restrict);
+
+#endif //HTTP_SERVER_STRINGS_H
