8d743c79602792528e7dec4e6f2f8622264fb490
Jujumba <far77578@gmail.com>
2023-04-26 11:11:12 +0200

    Implement map iterator using coroutine

---
+++ src/misc/iterator.c
@@ -0,0 +1,33 @@
+#include "iterator.h"
+
+iterator* new_iterator(hash_map* map) {
+    iterator* self = malloc(sizeof(iterator));
+    if (!self) {
+        PANIC("Malloc error\n");
+    }
+    self->map = map;
+    self->next = next;
+    self->index = 0;
+    self->node = NULL;
+    return self;
+}
+/* Tough to read coroutine implementation */
+const bucket_node* next(iterator* self) {
+    if (self->index == 0) {
+        goto coroutine_start;
+    } else if (self->index >= 1 && self->index < self->map->capacity) {
+        self->node = self->node->next;
+        goto coroutine_yield;
+    } else {
+        goto coroutine_end;
+    }
+coroutine_start:
+    for (; self->index < self->map->capacity;) {
+        self->node = self->map->buckets[self->index++];
+coroutine_yield:
+        if (!self->node) goto coroutine_start;
+        return self->node;
+    }
+coroutine_end:
+    return NULL;
+}
---
+++ src/misc/iterator.h
@@ -0,0 +1,18 @@
+#ifndef HTTP_SERVER_ITERATOR_H
+#define HTTP_SERVER_ITERATOR_H
+
+#include <stddef.h>
+#include "panic.h"
+#include "map.h"
+
+typedef struct iterator {
+    size_t index; /* Not intended to be edited manually  */
+    hash_map *map;
+    bucket_node *node;
+    const bucket_node* (*next)(struct iterator*);
+} iterator;
+
+iterator* new_iterator(hash_map*);
+const bucket_node* next(iterator*);
+
+#endif //HTTP_SERVER_ITERATOR_H
