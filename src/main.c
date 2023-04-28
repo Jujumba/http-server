223b5cbac63cbdde0a4b5d940682a44dec7e73b5
Jujumba <far77578@gmail.com>
2023-04-28 22:03:23 +0200

    main: add sample function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include "socket.h" 
 
+HttpResponse* sample(HttpRequest*) {
+    HttpResponse* res = malloc(sizeof(HttpResponse));
+    res->header = "HTTP/1.1 200 OK\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n\r\n";
+    res->header_len = strlen(res->header);
+    res->body = "<h1>Hello world!</h1>";
+    res->body_len = strlen(res->body);
+    return res;
+}
+
 int main() {
     HttpSocket* socket = create_socket(8888);
+    put_listener(socket, "/", sample);
     socket->start(socket);
     return 0;
 }
